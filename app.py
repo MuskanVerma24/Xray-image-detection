ca814f618c3a6b35a7ebeb310db3e21df71de593
Muskan Verma <163900143+MuskanVerma24@users.noreply.github.com>
2024-11-17 15:11:53 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,419 @@
+<<<<<<< HEAD
+import tensorflow as tf
+from tensorflow.keras.models import load_model
+import numpy as np
+import streamlit as st
+from PIL import Image
+import io
+import time
+
+
+st.markdown(
+        """
+        <style>
+        .stApp {
+                background-image: url("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxxkV_AVtgPh8j2gfH_UpwKPodmiQaXiarTw&s");
+                background-size: cover;
+                background-position: center;                
+        }
+        .stMainBlockContainer.ea3mdgi5{
+                max-width:100%;
+                padding: 0;
+                margin: 0;
+        }
+        header {
+                color: ; black
+                padding: 10px;
+        }
+        .header-background{
+                background-image: url("https://www.shutterstock.com/image-photo/xray-image-ankle-fracture-blue-260nw-2253312433.jpg");
+                background-size: cover;
+                background-position: center;
+                height: 100vh;
+                text-align: end;
+                align-content: center;
+                padding-right: 20px;
+                text-decoration:none;
+                clip-path: inset(0 0 35px 0);
+        }
+        .hero-section > p{
+                padding-right: 20px;
+        }
+        .stFileUploader {
+                width: 70%; 
+                margin: 0 auto;
+        }
+        .section-title {
+                color:white;
+                font-size: 30px;
+                font-weight: bold;
+                margin-top: 40px;
+                width: 70%;
+                margin: auto;
+        }
+        .section-subtitle {
+                color:white;
+                font-size: 18px;
+                font-weight: normal;
+                margin-top: -10px;
+                width: 70%;
+                margin: auto;
+                margin-bottom:10px;
+        }
+        .decoration{
+                text-align: center;
+                text-decoration: overline;
+                margin-top: 3%;
+                font-size: 30px;
+                font-weight: 800;
+        }
+        .stHorizontalBlock.st-emotion-cache-ocqkz7.e1f1d6gn5{
+                width: 70%;
+                margin: auto;
+        }
+        .st-emotion-cache-1xf0csu.e115fcil1 > img{
+                height: 300px;
+        }
+        #root > div:nth-child(1) > div.withScreencast > div > div > section > div.stMainBlockContainer.block-container.st-emotion-cache-13ln4jf.ea3mdgi5 > div > div > div > div:nth-child(5) > div.stColumn.st-emotion-cache-fplge5.e1f1d6gn3 > div{
+                width: 100%;
+        }
+        .footer {
+                color: #ffff;
+                background-color:black;
+                text-align: center;
+                margin-top: 50px;
+                font-size: 16px;
+                padding:12px;
+        }
+        </style>
+        """,
+        unsafe_allow_html=True
+)
+
+
+# CSS for floating effect
+st.markdown(
+    """
+    <style>
+    /* Floating animation */
+    @keyframes floatIn {
+        from {
+            transform: translateY(30px); 
+            opacity: 0;
+        }
+        to {
+            transform: translateY(0); 
+            opacity: 1;
+        }
+    }
+
+    /* Apply animation to wrapper div */
+    .floating-element {
+        animation: floatIn 1.5s ease-out forwards;
+        opacity: 0; 
+    }
+
+    /* Delay for staggered animation */
+    .floating-element-1 { animation-delay: 0.2s; }
+    .floating-element-2 { animation-delay: 0.4s; }
+    .floating-element-3 { animation-delay: 1.4s; }
+    .floating-element-4 { animation-delay: 1.8s; }
+
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+
+# header
+st.markdown('''<header class='header-background full-width-header '>
+                        <div class='hero-section floating-element floating-element-1'><h1>Fracture Detection in X-ray Images</h1>
+                                <p class='hero-section floating-element floating-element-2'>An AI-powered application to assist in detecting fractures from X-ray images.</p>
+                        </div>
+                </header>''', 
+               unsafe_allow_html=True)
+
+
+
+st.markdown("<div class='decoration floating-element floating-element-3'>Upload an X-ray Image </div>",unsafe_allow_html=True)
+
+# File uploader
+uploaded_file = st.file_uploader("Choose an X-ray image...", type=["jpg", "jpeg", "png"])
+
+model = load_model("model/recognition_model.keras")
+    
+data_label= ['fractured', 'not fractured']
+
+image_height = 256
+image_width = 256
+    
+# Run classification if an image is uploaded
+if uploaded_file is not None:
+        try:
+                # Columns layout
+                col1, col2 = st.columns([3, 2])
+
+                with col1:
+                        # spinner
+                        with st.spinner("Processing the uploaded image..."):
+                                time.sleep(1)
+
+                        # reading image
+                        image = Image.open(io.BytesIO(uploaded_file.read()))
+                        if image.mode != "RGB":
+                                image = image.convert("RGB")
+                        
+                        img_arr = np.array(image.resize((image_height,image_width)))
+                        img_arr = np.array(img_arr.reshape((1,image_height,image_width,3)))
+
+                        predict = model.predict(img_arr)
+
+                        # Use softmax to get probabilities for each class
+                        probabilities = tf.nn.softmax(predict)
+
+                        # Get the predicted class
+                        predicted_class = np.argmax(probabilities)
+
+                        # Display the uploaded image
+                        st.image(image, caption="Uploaded X-ray Image", use_container_width=True)
+
+                with col2:
+                        # Display Result
+                        st.subheader("Detection Result")
+                        
+                        st.write("The bone is : ", data_label[predicted_class] )
+
+                        st.write("This result is generated by our deep learning model, trained on X-ray images.")
+        except Exception as e:
+                st.error(f"An error occured :{e}")    
+
+
+
+
+# Example X-ray Images Section
+st.markdown("<div class='section-title floating-element floating-element-4'>Example X-ray Images</div>", unsafe_allow_html=True)
+st.markdown("<div class='section-subtitle'>See what the app detects</div>", unsafe_allow_html=True)
+col1, col2 = st.columns(2)
+with col1:
+        st.image(r"https://www.shutterstock.com/image-photo/blue-tone-radiograph-on-dark-600nw-2267523647.jpg", caption="Fractured X-ray", use_container_width=True)
+        
+with col2:
+        st.image(r"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpqwF54Bgvy8KwtTxT7W3mIEjH6MxqqMrYhg&s", caption="Normal X-ray", use_container_width=True)
+       
+
+
+
+# Footer 
+st.markdown("<div class='footer'>Trained on a dataset of X_ray images for fracture detection</div>", unsafe_allow_html=True)
+
+
+=======
+import tensorflow as tf
+from tensorflow.keras.models import load_model
+import numpy as np
+import streamlit as st
+from PIL import Image
+import io
+import time
+
+
+st.markdown(
+        """
+        <style>
+        .stApp {
+                background-image: url("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxxkV_AVtgPh8j2gfH_UpwKPodmiQaXiarTw&s");
+                background-size: cover;
+                background-position: center;                
+        }
+        .stMainBlockContainer.ea3mdgi5{
+                max-width:100%;
+                padding: 0;
+                margin: 0;
+        }
+        header {
+                color: ; black
+                padding: 10px;
+        }
+        .header-background{
+                background-image: url("https://www.shutterstock.com/image-photo/xray-image-ankle-fracture-blue-260nw-2253312433.jpg");
+                background-size: cover;
+                background-position: center;
+                height: 100vh;
+                text-align: end;
+                align-content: center;
+                padding-right: 20px;
+                text-decoration:none;
+                clip-path: inset(0 0 35px 0);
+        }
+        .hero-section > p{
+                padding-right: 20px;
+        }
+        .stFileUploader {
+                width: 70%; 
+                margin: 0 auto;
+        }
+        .section-title {
+                color:white;
+                font-size: 30px;
+                font-weight: bold;
+                margin-top: 40px;
+                width: 70%;
+                margin: auto;
+        }
+        .section-subtitle {
+                color:white;
+                font-size: 18px;
+                font-weight: normal;
+                margin-top: -10px;
+                width: 70%;
+                margin: auto;
+                margin-bottom:10px;
+        }
+        .decoration{
+                text-align: center;
+                text-decoration: overline;
+                margin-top: 3%;
+                font-size: 30px;
+                font-weight: 800;
+        }
+        .stHorizontalBlock.st-emotion-cache-ocqkz7.e1f1d6gn5{
+                width: 70%;
+                margin: auto;
+        }
+        .st-emotion-cache-1xf0csu.e115fcil1 > img{
+                height: 300px;
+        }
+        #root > div:nth-child(1) > div.withScreencast > div > div > section > div.stMainBlockContainer.block-container.st-emotion-cache-13ln4jf.ea3mdgi5 > div > div > div > div:nth-child(5) > div.stColumn.st-emotion-cache-fplge5.e1f1d6gn3 > div{
+                width: 100%;
+        }
+        .footer {
+                color: #ffff;
+                background-color:black;
+                text-align: center;
+                margin-top: 50px;
+                font-size: 16px;
+                padding:12px;
+        }
+        </style>
+        """,
+        unsafe_allow_html=True
+)
+
+
+# CSS for floating effect
+st.markdown(
+    """
+    <style>
+    /* Floating animation */
+    @keyframes floatIn {
+        from {
+            transform: translateY(30px); 
+            opacity: 0;
+        }
+        to {
+            transform: translateY(0); 
+            opacity: 1;
+        }
+    }
+
+    /* Apply animation to wrapper div */
+    .floating-element {
+        animation: floatIn 1.5s ease-out forwards;
+        opacity: 0; 
+    }
+
+    /* Delay for staggered animation */
+    .floating-element-1 { animation-delay: 0.2s; }
+    .floating-element-2 { animation-delay: 0.4s; }
+    .floating-element-3 { animation-delay: 1.4s; }
+    .floating-element-4 { animation-delay: 1.8s; }
+
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+
+# header
+st.markdown('''<header class='header-background full-width-header '>
+                        <div class='hero-section floating-element floating-element-1'><h1>Fracture Detection in X-ray Images</h1>
+                                <p class='hero-section floating-element floating-element-2'>An AI-powered application to assist in detecting fractures from X-ray images.</p>
+                        </div>
+                </header>''', 
+               unsafe_allow_html=True)
+
+
+
+st.markdown("<div class='decoration floating-element floating-element-3'>Upload an X-ray Image </div>",unsafe_allow_html=True)
+
+# File uploader
+uploaded_file = st.file_uploader("Choose an X-ray image...", type=["jpg", "jpeg", "png"])
+
+model = load_model("model/recognition_model.keras")
+    
+data_label= ['fractured', 'not fractured']
+
+image_height = 256
+image_width = 256
+    
+# Run classification if an image is uploaded
+if uploaded_file is not None:
+        try:
+                # Columns layout
+                col1, col2 = st.columns([3, 2])
+
+                with col1:
+                        # spinner
+                        with st.spinner("Processing the uploaded image..."):
+                                time.sleep(1)
+
+                        # reading image
+                        image = Image.open(io.BytesIO(uploaded_file.read()))
+                        if image.mode != "RGB":
+                                image = image.convert("RGB")
+                        
+                        img_arr = np.array(image.resize((image_height,image_width)))
+                        img_arr = np.array(img_arr.reshape((1,image_height,image_width,3)))
+
+                        predict = model.predict(img_arr)
+
+                        # Use softmax to get probabilities for each class
+                        probabilities = tf.nn.softmax(predict)
+
+                        # Get the predicted class
+                        predicted_class = np.argmax(probabilities)
+
+                        # Display the uploaded image
+                        st.image(image, caption="Uploaded X-ray Image", use_container_width=True)
+
+                with col2:
+                        # Display Result
+                        st.subheader("Detection Result")
+                        
+                        st.write("The bone is : ", data_label[predicted_class] )
+
+                        st.write("This result is generated by our deep learning model, trained on X-ray images.")
+        except Exception as e:
+                st.error(f"An error occured :{e}")    
+
+
+
+
+# Example X-ray Images Section
+st.markdown("<div class='section-title floating-element floating-element-4'>Example X-ray Images</div>", unsafe_allow_html=True)
+st.markdown("<div class='section-subtitle'>See what the app detects</div>", unsafe_allow_html=True)
+col1, col2 = st.columns(2)
+with col1:
+        st.image(r"https://www.shutterstock.com/image-photo/blue-tone-radiograph-on-dark-600nw-2267523647.jpg", caption="Fractured X-ray", use_container_width=True)
+        
+with col2:
+        st.image(r"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpqwF54Bgvy8KwtTxT7W3mIEjH6MxqqMrYhg&s", caption="Normal X-ray", use_container_width=True)
+       
+
+
+
+# Footer 
+st.markdown("<div class='footer'>Trained on a dataset of X_ray images for fracture detection</div>", unsafe_allow_html=True)
+
+
+>>>>>>> 76c88096052bf46173b45961d166941129eb438c
---
+++ model/training.py
@@ -0,0 +1,89 @@
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from tensorflow.keras import layers ,models
+
+
+data_train_path = "Bone_Fracture_Binary_Classification/Bone_Fracture_Binary_Classification/train"
+data_test_path = "Bone_Fracture_Binary_Classification/Bone_Fracture_Binary_Classification/test"
+data_val_path = "Bone_Fracture_Binary_Classification/Bone_Fracture_Binary_Classification/val"
+
+img_width = 256
+img_height = 256
+
+# Function to load dataset with error handling
+def load_dataset(path):
+    dataset = tf.keras.utils.image_dataset_from_directory(
+        path,
+        shuffle=True,
+        image_size=(img_width, img_height),
+        batch_size=32,
+        validation_split=False
+    )
+    return dataset.apply(tf.data.experimental.ignore_errors())
+
+
+
+# Load datasets with error handling
+data_train = load_dataset(data_train_path)
+data_val = load_dataset(data_val_path)
+data_test = load_dataset(data_test_path)
+
+
+# Build CNN model 
+model = models.Sequential([
+
+    layers.Conv2D(32, kernel_size=(3,3), padding='valid',activation='relu',input_shape=(256,256,3)),
+    layers.BatchNormalization(),
+    layers.MaxPooling2D(pool_size=(2,2),strides=2,padding='valid'),
+
+    layers.Conv2D(32, kernel_size=(3,3), padding='valid',activation='relu'),
+    layers.BatchNormalization(),
+    layers.MaxPooling2D(pool_size=(2,2),strides=2,padding='valid'),
+
+    layers.Conv2D(32, kernel_size=(3,3), padding='valid',activation='relu'),
+    layers.BatchNormalization(),
+    layers.MaxPooling2D(pool_size=(2,2),strides=2,padding='valid'),
+
+    layers.Flatten(),
+
+    layers.Dense(128,activation='relu'),
+    layers.Dropout(0.1),
+
+    layers.Dense(64,activation='relu'),
+    layers.Dropout(0.1),
+
+    layers.Dense(2, activation='sigmoid')
+])
+
+
+# Compile model
+model.compile(optimizer='adam', 
+              loss= 'sparse_categorical_crossentropy', 
+              metrics=['accuracy'])
+
+
+# Train model
+history = model.fit(data_train,validation_data=data_val, batch_size=32, epochs=10)
+
+
+# Plot training & validation accuracy
+plt.plot(history.history['accuracy'], label='accuracy')
+plt.plot(history.history['val_accuracy'], label='val_accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.ylim([0, 1])
+plt.legend(loc='lower right')
+plt.show()
+
+
+# Evaluate model on test data
+test_loss, test_acc = model.evaluate(data_test, verbose=2)
+print(f"Test accuracy: {test_acc* 100:.2f}%")
+
+
+# save the model
+model.save("model/recognition_model.keras")
+
+
+
+
